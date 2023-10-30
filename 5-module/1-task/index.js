003c26f8e84a15d5ae8a61f7c922605abaf69b42
Vlad Maznev <votpuskedruzya@gmail.com>
2023-10-30 13:51:59 +0300

    Completed DAY-5

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,8 @@
 function hideSelf() {
-  // ваш код...
+  let button = document.querySelector('.hide-self-button');
+
+  button.addEventListener('click', () => button.hidden = true);
 }
+// почему в консоле браузера при введении "button", 
+//для просмотра содержимого, выводит ошибку:
+//ReferenceError: Can't find variable: button
--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector('.toggle-text-button');
+  let text = document.querySelector('#text');
+
+  button.addEventListener('click', function() {
+    if (text.hidden == false) {
+      text.hidden = true;
+    } else if (text.hidden == true) {
+      text.hidden = false; 
+    } 
+  }); 
 }
