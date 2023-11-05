da1ec37c7756c7a9722bbe1f79684276d82af112
Vlad Maznev <votpuskedruzya@gmail.com>
2023-11-06 02:45:10 +0300

    Completed TASK-2-MODULE-6

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,31 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class ProductCard {
   constructor(product) {
+    this.elem = createElement(`
+      <div class="card">
+      <div class="card__top">
+          <img src="/assets/images/products/${product.image}" class="card__image" alt="product">
+          <span class="card__price">€${product.price.toFixed(2)}</span>
+      </div>
+      <div class="card__body">
+          <div class="card__title">${product.name}</div>
+          <button type="button" class="card__button">
+              <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+      </div>
+      </div>
+    `)
+
+    //често скажу, всё что дальше я не до конца понимаю,
+    //сидел тыкал час, и случайно получилось 
+    //наверно вопрос в том, как вообще это работает...
+    const plus = this.elem.querySelector('.card__button');
+    plus.addEventListener('click', () => {
+      this.elem.dispatchEvent(new CustomEvent('product-add', {
+        detail: product.id,
+        bubbles: true
+      }))
+    });
   }
 }
