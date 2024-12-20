8a4fe077e5230cafe6eda511506f16ee4e6fb565
Vlad Maznev <votpuskedruzya@gmail.com>
2024-12-20 23:40:49 +0300

    7-1-task done

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,8 +1,5 @@
 function hideSelf() {
-  let button = document.querySelector('.hide-self-button');
+  let button = document.querySelector(".hide-self-button");
 
-  button.addEventListener('click', () => button.hidden = true);
+  button.addEventListener("click", () => (button.hidden = true));
 }
-// почему в консоле браузера при введении "button", 
-//для просмотра содержимого, выводит ошибку:
-//ReferenceError: Can't find variable: button
--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -1,7 +1,93 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
+    this.elem = this.#render();
+    this.#eventArrows();
+    this.#eventSelect();
+  }
+
+  #render() {
+    const ribbonMenu = createElement(`
+      <div class="ribbon">
+    <button class="ribbon__arrow ribbon__arrow_left ribbon__arrow_visible">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </button>
+    <nav class="ribbon__inner">
+    <a href="#" class="ribbon__item ribbon__item_active" data-id="">All</a>
+      <a href="#" class="ribbon__item" data-id="salads">Salads</a>
+      <a href="#" class="ribbon__item" data-id="soups">Soups</a>
+      <a href="#" class="ribbon__item" data-id="chicken-dishes">Chicken dishes</a>
+      <a href="#" class="ribbon__item" data-id="beef-dishes">Beef dishes</a>
+      <a href="#" class="ribbon__item" data-id="seafood-dishes">Seafood dishes</a>
+      <a href="#" class="ribbon__item" data-id="vegetable-dishes">Vegetable dishes</a>
+      <a href="#" class="ribbon__item" data-id="bits-and-bites">Bits and bites</a>
+      <a href="#" class="ribbon__item" data-id="on-the-side ribbon__item_active">On the side</a>
+    </nav>
+     <button class="ribbon__arrow ribbon__arrow_right">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </button>
+    </div>
+    `);
+
+    return ribbonMenu;
+  }
+
+  #eventArrows() {
+    const arrowLeft = this.elem.querySelector(".ribbon__arrow_left");
+    const arrowRight = this.elem.querySelector(".ribbon__arrow_right");
+    const ribbonInner = this.elem.querySelector(".ribbon__inner");
+
+    arrowLeft.classList.remove("ribbon__arrow_visible");
+    arrowRight.classList.add("ribbon__arrow_visible");
+
+    arrowLeft.addEventListener("click", () => {
+      ribbonInner.scrollBy(-350, 0);
+    });
+
+    arrowRight.addEventListener("click", () => {
+      ribbonInner.scrollBy(350, 0);
+    });
+
+    ribbonInner.addEventListener("scroll", () => {
+      let scrollLeft = ribbonInner.scrollLeft;
+      let scrollWidth = ribbonInner.scrollWidth;
+      let clientWidth = ribbonInner.clientWidth;
+      let scrollRight = scrollWidth - scrollLeft - clientWidth;
+      if (scrollLeft > 0) {
+        arrowLeft.classList.add("ribbon__arrow_visible");
+      } else {
+        arrowLeft.classList.remove("ribbon__arrow_visible");
+      }
+
+      if (scrollRight < 1) {
+        arrowRight.classList.remove("ribbon__arrow_visible");
+      } else {
+        arrowRight.classList.add("ribbon__arrow_visible");
+      }
+    });
+  }
+
+  #eventSelect() {
+    const ribbonItems = this.elem.querySelectorAll(".ribbon__item");
+
+    ribbonItems.forEach((item, index) => {
+      item.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        ribbonItems.forEach((el) => el.classList.remove("ribbon__item_active"));
+
+        item.classList.add("ribbon__item_active");
+
+        const category = this.categories[index];
+        this.elem.dispatchEvent(
+          new CustomEvent("ribbon-select", {
+            detail: category.id,
+            bubbles: true,
+          })
+        );
+      });
+    });
   }
 }
